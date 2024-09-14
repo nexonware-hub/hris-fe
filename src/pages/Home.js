4dcf278a74e75bb90646e2797a27fda3e97707d7
Romita Sharma <romitasharma@Romitas-MacBook-Air.local>
2024-09-14 23:51:21 +0530

    pwa icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -98,7 +98,7 @@ const Home = () => {
         alignItems: 'center',
         paddingTop: '60px',
         paddingBottom: '15px',
-        paddingLeft: '70px',
+        paddingLeft: '40px',
         paddingRight: '15px',
         flexWrap: 'wrap',
         fontSize: '14px',
